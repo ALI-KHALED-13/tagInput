e42d788a37c96eb3876ef5e3365240805a4080aa
Ali <aliknake@gmail.com>
2021-10-10 16:21:09 +0200

    code readability enhancment final

--- src/TagInput.js
+++ src/TagInput.js
@@ -3,7 +3,7 @@ import { useState } from "react"
 const TagInput =({savedTags, saveTag})=>{
     const [isFocused, setIsFocused] = useState(false);
     const [tagInp, setTagInp] = useState('');
-    const [tagsChosen, setTagsChosen] = useState([]);
+    const [addedTags, setAddedTags] = useState([]);
 
     const handleChange =(ev)=>{
         if (ev.target.value === ' ') return; // to avoid storing /s
@@ -16,46 +16,41 @@ const TagInput =({savedTags, saveTag})=>{
         if (!tagInp) return;
 
         if ( !ev.code || ev.code === 'Enter' || ev.code === 'Space' ){
-            const tagTxt = ev.target.className !== 'suggestion'? tagInp : ev.target.textContent;
-            if (tagsChosen.indexOf(tagTxt) >= 0) return setTagInp(''); //if already added reset
+            const tagTxt = ev.target.className !== 'suggestion'? tagInp : ev.target.textContent; //async setTagInp
+            if (addedTags.indexOf(tagTxt) >= 0) return setTagInp(''); //if already added reset
 
             saveTag(tagTxt);
 
-            setTagsChosen([...tagsChosen, tagTxt]);
+            setAddedTags([...addedTags, tagTxt]);
 
             setTagInp('');
         }
     }
 
-    const removeFromBag =(ev)=> setTagsChosen(tagsChosen.filter(tag=> tag !== ev.target.previousElementSibling.textContent));
+    const removeFromBag =(ev)=> setAddedTags(addedTags.filter(tag=> tag !== ev.target.previousElementSibling.textContent));
 
-    const insertSuggestions =()=> { //custom hook next
+    const insertSuggestions =()=> { 
         if (!tagInp) return;
 
         const hints = savedTags.filter(tag=> tag.startsWith(tagInp));
         
         if ( !hints.length ) return <div className="add" onClick={addToBag}>+</div>;
         
-        return hints.map((tag, ind)=> (
-              <div
-                className="suggestion" 
-                key={Date.now() + ind}
-                onClick={addToBag}
-              >
-                {tag}
-              </div>
-            ) );
+        return hints.map((tag, ind)=> <div className="suggestion" key={Date.now() + ind} onClick={addToBag}> {tag} </div>);
     }
 
     return (
         <section>
+
             <div id="tagsBag">
                 {
-                tagsChosen.map((tag, ind)=> (
-                    <div className="tag" key={Date.now() + ind}>
-                        <p>{tag}</p> <span className="del" onClick={removeFromBag}>X</span>
-                    </div>)
-                    )
+                    addedTags.map((tag, ind)=> {
+                        return (
+                            <div className="tag" key={Date.now() + ind}>
+                                <p>{tag}</p> <span className="del" onClick={removeFromBag}>X</span>
+                            </div>
+                        )
+                    })
                 }
             </div>
 
@@ -69,9 +64,10 @@ const TagInput =({savedTags, saveTag})=>{
 
             <div id="suggestions">
                 {
-                isFocused && insertSuggestions()
+                    isFocused && insertSuggestions()
                 }
             </div>
+            
         </section>
     );
 }
